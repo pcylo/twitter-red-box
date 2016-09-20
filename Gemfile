2f2e680370452eac3798682fb7e30f1188a0db92
pcylo <pawel@firma.xin.pl>
2016-09-20 16:45:14 +0200

    Add Pry debugger

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.5'
+  gem 'pry-byebug', '~> 3.4.0'
 end
 
 group :development do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
+    @user = User.from_omniauth(auth_hash)
     session[:user_id] = @user.id
     flash[:success] = "Welcome, #{@user.name}!"
     redirect_to root_path
