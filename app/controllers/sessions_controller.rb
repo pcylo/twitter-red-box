7609655171ac66d1bf0d9bd9aba3617a4ed46124
Paweł Cyło <pawel@firma.xin.pl>
2016-09-20 09:12:39 +0200

    Fix indent

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-	def create
+  def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
     redirect_to root_path
